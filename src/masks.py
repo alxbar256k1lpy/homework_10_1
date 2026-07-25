38188cde4f1d22ca30508b5eaf9f0a1166fd03c5
Alexandr Barantsev <alexbarxxi@gmail.com>
2026-07-25 20:02:59 +0200

    функции get_mask_card_number и get_mask_account

---
+++ src/masks.py
@@ -0,0 +1,23 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскировки номера банковской карты"""
+
+    card_number_str = str(card_number)
+    if len(card_number_str) != 16:
+        return "Некорректный номер карты"
+
+    return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+
+
+def get_mask_account(mask_account: str) -> str:
+    """Функция маскировки номера банковского счета"""
+
+    mask_account_str = str(mask_account)
+    if len(mask_account_str) != 20:
+        return "Некорректный номер счета"
+
+    return f"**{mask_account_str[-4:]}"
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("7000792289606361"))
+    print(get_mask_account("73654108430135874305"))
